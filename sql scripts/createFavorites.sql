5a7f910779123bb52c3a1ba7479500d5699b8773
noa-blenkitny <noa.blenkitny@gmail.com>
2022-06-07 20:17:47 +0300

    added tables to db

---
+++ sql scripts/createFavorites.sql
@@ -0,0 +1,9 @@
+
+
+
+CREATE TABLE IF NOT EXISTS mydb.FavoriteRecipes(
+    user_id int,
+    recipe_id VARCHAR(45),
+    PRIMARY KEY (user_id,recipe_id),
+    FOREIGN KEY (user_id) REFERENCES mydb.users(user_id)
+);
---
+++ sql scripts/createUsers.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE IF NOT EXISTS mydb.users(
+    user_id int AUTO_INCREMENT PRIMARY KEY,
+    username varchar(45),
+    firstname varchar(45),
+    lastname varchar(45),
+    country varchar(45),
+    password varchar(100),
+    email varchar(45)
+);
